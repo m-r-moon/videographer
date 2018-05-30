033b8a0b8b906fb8a0a72b7a2a8f225416674f67
mrmoon <mrmoon@una.edu>
2018-05-29 19:24:44 -0500

    initial commit

---
+++ credentials.py
@@ -0,0 +1,9 @@
+###############################
+# Filename: credentials.py    #
+# Author: Mitchel R Moon      #
+###############################
+
+access_token = ''
+access_secret = ''
+consumer_key = ''
+consumer_secret = ''
